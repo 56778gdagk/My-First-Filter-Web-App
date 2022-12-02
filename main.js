6427e54e04144373558bfff28d84c434051f532b
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2022-12-02 17:58:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,31 @@ function preload()
 
 function setup()
 {
-    
+    cnavas = createCanvas(200,200);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(200,200);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modeLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function  draw()
 {
 
+}
+
+function modeLoaded()
+{
+    console.log('model Loaded')
+}
+
+function gotPoses(results)
+{
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
 }
